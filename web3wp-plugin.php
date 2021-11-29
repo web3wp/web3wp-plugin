59825acc6da6923635a5b4cf079e08060a95d91b
Rheinard Korf <rheinard.korf@xwp.co>
2021-11-29 23:51:06 +1100

    Initial setup.

---
+++ web3wp-plugin.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Web3WP Plugin.
+ *
+ * @package   Web3WP
+ * @copyright Copyright(c) 2021, Rheinard Korf
+ * @licence https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License, version 3 (GPLv3)
+ *
+ * Plugin Name: Web3WP
+ * Plugin URI: https://github.com/web3wp/web3wp-plugin
+ * Description: Authenticate users using a Web3 wallet, like MetaMask.
+ * Version: 0.1.0
+ * Author: Rheinard Korf
+ * Author URI: https://rheinardkorf.com
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: wallet-connect
+ */
